4c686855184ed4b94566331bcf1d1af4e8ebe77c
Neel Adwani <nprosofficial@gmail.com>
2021-06-29 06:21:40 +0000

    Game in a loop

--- main.py
+++ main.py
@@ -4,20 +4,21 @@ import random
 
 cards = ['13', '12', '11', '10', '9', '8', '7', '6', '5', '4', '3', '2', '1']
 
-random.shuffle(cards)
+while True:
+  random.shuffle(cards)
 
-p1 = int(input("Player 1: "))
-p2 = int(input("Player 2: "))
+  p1 = int(input("Player 1: "))
+  p2 = int(input("Player 2: "))
 
-c1 = cards[p1]
-c2 = cards[p2]
+  c1 = cards[p1]
+  c2 = cards[p2]
 
-print("Card picked by Player 1: " + c1)
-print("Card picked by Player 2: " + c2)
+  print("Card picked by Player 1: " + c1)
+  print("Card picked by Player 2: " + c2)
 
-if c1 > c2:
-  print("Player 1 is the winner!")
-elif c1 < c2:
-  print("Player 2 is the winner!")
-else:
-  print("It's a tie!")
+  if c1 > c2:
+    print("Player 1 is the winner!")
+  elif c1 < c2:
+    print("Player 2 is the winner!")
+  else:
+    print("It's a tie!")
